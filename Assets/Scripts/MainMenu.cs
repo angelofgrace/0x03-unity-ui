274a6c1ff9b0391fe674f6dc5ca05281f8694603
Grace Fallon <graceleefallon@gmail.com>
2021-08-30 18:22:32 -0500

    create options menu and toggle back to main

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,12 +9,19 @@ public class MainMenu : MonoBehaviour
     public Scene mazeScene;
     public Button playButton;
     public Button quitButton;
+    public Button optionsButton;
+    public Button backButton;
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
 
     // Start is called before the first frame update
     void Start()
     {
         playButton.onClick.AddListener(PlayMaze);
         quitButton.onClick.AddListener(QuitMaze);
+        optionsButton.onClick.AddListener(OptionsMenuOpen);
+        backButton.onClick.AddListener(MainMenuOpen);
     }
 
     // Update is called once per frame
@@ -31,5 +38,17 @@ public class MainMenu : MonoBehaviour
     public void QuitMaze()
     {
         Debug.Log("Quit Game");
+    }
+
+    public void OptionsMenuOpen()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void MainMenuOpen()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
